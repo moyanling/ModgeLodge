9c0a55ab4c6341bb93f5b3266098ecb235e02cb3
Jihan Chen <mo39.fmbh@gmail.com>
2018-12-06 20:08:35 -0500

    Save work

--- MNIST/src/main/scala/org/mo39/fmbh/mnist/MNIST.scala
+++ MNIST/src/main/scala/org/mo39/fmbh/mnist/MNIST.scala
@@ -1,10 +1,15 @@
 package org.mo39.fmbh.mnist
 
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import java.util.Base64
+
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator
-import org.deeplearning4j.eval.Evaluation
+import org.nd4j.evaluation.classification.Evaluation
 import org.deeplearning4j.nn.api.OptimizationAlgorithm
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration
-import org.deeplearning4j.nn.conf.layers.{DenseLayer, OutputLayer}
+import org.deeplearning4j.nn.conf.layers.{ DenseLayer, OutputLayer }
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
@@ -13,10 +18,35 @@ import org.nd4j.linalg.dataset.api.iterator.DataSetIterator
 import org.nd4j.linalg.learning.config.Sgd
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction
 import com.typesafe.scalalogging.LazyLogging
+import javax.imageio.ImageIO
+import org.nd4j.linalg.api.ndarray.INDArray
 
-import scala.collection.JavaConverters.asScalaIteratorConverter
+import scala.collection.JavaConverters._
 
 object MNIST extends App with LazyLogging {
+
+  def mkBase64ImgUrl(array: INDArray): String = {
+    // Check array size
+    require(array.rank() == 2)
+    require(array.rows() * array.columns() == 28 * 28)
+    // Convert to 28 * 28 array
+    val arr = array
+      .reshape(28, 28)
+      .toDoubleMatrix
+      .map(_.map(v => 255 - (v * 255).toInt))
+    // Build BufferedImage
+    val img = new BufferedImage(28, 28, BufferedImage.TYPE_BYTE_GRAY)
+    for (i <- 0 until 28) {
+      for (j <- 0 until 28) {
+        val v = arr(i)(j)
+        img.setRGB(j, i, new Color(v, v, v).getRGB)
+      }
+    }
+    // BufferedImage to base64 string
+    val os: ByteArrayOutputStream = new ByteArrayOutputStream()
+    ImageIO.write(img, "jpg", os)
+    "data:image/jpeg;base64," + Base64.getEncoder.encodeToString(os.toByteArray)
+  }
 
   val seed = 42 // fixed random seed for reproducibility
   val numInputs = 28 * 28
@@ -26,9 +56,18 @@ object MNIST extends App with LazyLogging {
   val batchSize = 128
   val numEpochs = 10
 
-  // download and load the MNIST images as tensors
+//   download and load the MNIST images as tensors
   val mnistTrain = new MnistDataSetIterator(batchSize, true, seed)
   val mnistTest = new MnistDataSetIterator(batchSize, false, seed)
+
+  val list = mnistTrain.asScala.toList
+  val arr = list.head.getFeatures.getRow(0)
+//  println(mkBase64ImgUrl(arr))
+//  println(list.head.getLabels.getRow(0))
+
+  val sec = list.head.getFeatures.getRow(1)
+  println(mkBase64ImgUrl(sec))
+  println(list.head.getLabels.getRow(1))
 
   // define the neural network architecture
   val conf = new NeuralNetConfiguration.Builder()
@@ -73,5 +112,4 @@ object MNIST extends App with LazyLogging {
 
   logger.info(s"Train accuracy = ${accuracy(mnistTrain)}")
   logger.info(s"Test accuracy = ${accuracy(mnistTest)}")
-
 }
--- build.sbt
+++ build.sbt
@@ -1,4 +1,9 @@
-name := "ModgeLodge"
+name := """ModgeLodge"""
+
+resolvers in ThisBuild ++= Seq(
+  Resolver.mavenCentral,
+  Resolver.sonatypeRepo("snapshots")
+)
 
 lazy val commonSettings = Seq(
   organization := "org.mo39.fmbh",
@@ -6,19 +11,33 @@ lazy val commonSettings = Seq(
   scalaVersion := "2.12.8"
 )
 
-resolvers in ThisBuild ++= Seq(
-  Resolver.mavenCentral,
-  Resolver.sonatypeRepo("snapshots")
-)
+lazy val root = (project in file("."))
+  .settings(commonSettings: _*)
+  .enablePlugins(PlayScala)
+  .aggregate(MNIST)
+  .dependsOn(MNIST)
 
+///////////////////////////////////////
+////////////Project Common/////////////
+///////////////////////////////////////
+lazy val common = (project in file("common"))
+  .settings(commonSettings)
+  .settings(
+    name := "modgelodge-common"
+  )
+
+///////////////////////////////////////
+////////////Project MNIST//////////////
+///////////////////////////////////////
 lazy val MNIST = (project in file("MNIST"))
+  .dependsOn(common)
   .settings(commonSettings: _*)
   .settings(
     name := "MNIST",
     libraryDependencies ++= Seq(
-      "org.nd4j" % "nd4j-native-platform" % "1.0.0-beta3",
-      "org.deeplearning4j" % "deeplearning4j-core" % "1.0.0-beta3",
-      "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0",
-      "ch.qos.logback" % "logback-classic" % "1.2.3"
+      "org.deeplearning4j" % "deeplearning4j-core" % "1.0.0-beta3", // DeepLearning for Java
+      "org.nd4j" % "nd4j-native-platform" % "1.0.0-beta3", //  DeepLearning for Java backend
+      "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0", // Logging
+      "ch.qos.logback" % "logback-classic" % "1.2.3" // Logging backend
     )
   )
